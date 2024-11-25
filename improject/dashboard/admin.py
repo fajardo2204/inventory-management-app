b3ed433ffc2ec69fcfc4d6a57ae70390b738e88f
fajardo2204 <juanfa_2204@hotmail.es>
2024-11-25 12:05:44 -0500

    modelo de ordenes creado, clase meta para modelos generado y todos los modelos de datos migrados

--- improject/dashboard/admin.py
+++ improject/dashboard/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 admin.site.site_header = 'Inventory Management System Admin Panel'
 
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ['name', 'category', 'quantity']
+  list_filter = ['category']
+  search_fields = ['name', 'category']
+
 # Register your models here.
 
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
 
 # admin.site.unregister(Group)
--- improject/dashboard/models.py
+++ improject/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORY_CHOICES = [
@@ -8,10 +9,27 @@ CATEGORY_CHOICES = [
   ('admin', 'admin'),
 ]
 
+# Product model
 class Product(models.Model):
   name = models.CharField(max_length=100, null=False)
   category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, null=False)
   quantity = models.PositiveIntegerField(null=False)
 
+  class Meta:
+    verbose_name_plural = 'Productos'
+
   def __str__(self):
-    return f'{self.name}'
+    return f'{self.name} ({self.quantity})'
+
+# Order model
+class Order(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  staff = models.ForeignKey(User, models.CASCADE, null=True)
+  quantity = models.PositiveIntegerField(null=True)
+  date = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    verbose_name_plural = 'Ordenes'
+
+  def __str__(self):
+    return f'{self.product} ordenada por {self.staff.username}'
